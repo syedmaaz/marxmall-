80f779e5fdfb4c35809953cce65a6a62f9ad5816
shumailkhan2008@yahoo.com <shumailakhan2008@yahoo.com>
2016-01-20 21:16:44 +0500

    last push

--- frontend/views/home/index.php
+++ frontend/views/home/index.php
@@ -42,41 +42,8 @@
       </div>
     </div>
   </div>
-<!-- slider -->
-<!--
-<div class="row carousel-holder">
 
-    <div class="col-md-12">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-                <div class="item">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-                <div class="item">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-            </div>
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-        </div>
-    </div>
-
-</div> -->
-<!-- end slider -->
-
-<!-- cutomer favorities -->
+<!-- customer favourites -->
   <section class="block">
     <div class="container header-background">
       <div class="col-md-12">
@@ -119,7 +86,7 @@
 	</section>
 			
 </div>
-<!-- END cutomer favorities -->
+<!-- END customer favourites -->
 
 <!-- category collection -->
 
@@ -461,7 +428,19 @@ $(document).ready(function() {
 	fullWidth:"on",
 	fullScreen:"off",
 	fullScreenOffsetContainer: ""
-	}); 	
+	}); 
+
+   $("#best-seller-5").owlCarousel({
+	autoPlay :true,
+	stopOnHover : true,
+	navigation:true,
+	goToFirstSpeed : 2000,
+	slideSpeed:1500,
+	singleItem : true,
+	autoHeight : true,
+	pagination:false,
+	transitionStyle:"goDown"
+	});	
 }); 
 
 </script>
